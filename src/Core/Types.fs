29f898c64246e80ef639da1309dd8fe94e20d486
gretmn102 <erlgreywatson@gmail.com>
2025-02-10 12:48:16 +0200

    feat(core): write main types
    Closes: #2

---
+++ src/Core/Types.fs
@@ -0,0 +1,20 @@
+﻿namespace MarkdownCyoa.Core
+open Farkdown.SyntaxTree
+
+type ParagraphId = string
+
+type Action =
+    {
+        Reference: ParagraphId
+        Description: Line
+    }
+
+type Paragraph =
+    {
+        Id: ParagraphId
+        Title: Line
+        Main: Statement list
+        Actions: Action list
+    }
+
+type Document = Paragraph list
